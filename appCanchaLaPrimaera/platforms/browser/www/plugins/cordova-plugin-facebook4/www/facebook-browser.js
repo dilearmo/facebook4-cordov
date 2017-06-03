45708805c7e2248616ea771916bc015ae8ded704
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-03 23:10:35 +0000

    facebook4 installed

--- appCanchaLaPrimaera/platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js
+++ appCanchaLaPrimaera/platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js
@@ -142,7 +142,7 @@ if (window.location.protocol === "file:") {
 } else {
   window.fbAsyncInit = function() {
     FB.init({
-      appId      : APP_ID,  // APP_ID is populated by the cordova after_prepare hook
+      appId      : 1061872230622284,  // 1061872230622284 is populated by the cordova after_prepare hook
       xfbml      : true,
       version    : 'v2.7'
     });
