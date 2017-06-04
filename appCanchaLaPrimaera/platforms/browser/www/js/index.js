17aab0945f8d8a6cdcec3e6f95f4a63006e1b185
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-04 01:09:13 +0000

    Facebook4 plugin working

--- appCanchaLaPrimaera/platforms/browser/www/js/index.js
+++ appCanchaLaPrimaera/platforms/browser/www/js/index.js
@@ -27,22 +27,13 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        facebookConnectPlugin.getLoginStatus( 
-            function(result) {
-                // success
-                alert(result.status);
-                
-            },
-            function() {
-                // error
-            });
+        //this.receivedEvent('deviceready');
             
     },
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
+       /* var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
@@ -50,20 +41,75 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        */
+        
+        
     }
 };
 
 app.initialize();
 
-function ready(result) {
-    facebookConnectPlugin.login("email", 
-    function() {
-        // success
-        alert('success');
-        alert(result.state);
-        
-    },
-    function() {
-        alert('error');
+
+var scopes = 'email';
+
+var statusChangeCallback = function(response, callback) {
+    console.info('statudChangeCallback');
+    if(response.status === 'connected') {
+        getFacebookData();
+    } else {
+        //document.getElementById('status').innerHTML = 'Por favor, inicie sesión en Facebook';
+        callback(false);
+    }
+}
+
+var checkLoginState = function(callback) {
+    console.info('checkLoginState');
+    FB.getLoginStatus(function(response) {
+        console.info('FB.getLoginStatus');
+        statusChangeCallback(response, function(data) {
+            callback(data);
+        });
+    });  
+}
+
+var getFacebookData = function() {
+    console.info('getFacebookData');
+    FB.api('/me', {fields: 'name,email'}, function(response) {
+        // guardar sesi[on]
+        console.info('FB.api obteniendo datos');
+        console.error('Bienvenido ' + response.name + ' ' + response.id + ' ' + response.email);
+        var img = document.createElement('img');
+        img.setAttribute('src', 'http://graph.facebook.com/' + response.id + '/picture/type=large');
+        var imgDiv = document.getElementById('img'); 
+        imgDiv.appendChild(img);
     });
+}
+
+var facebookLogin = function() {
+    console.info('facebookLogin');
+    checkLoginState(function(response) {
+        if(!response) {
+            FB.login(function(response) {
+                if(response.status === 'connected') {
+                    getFacebookData();
+                }
+            }, { scope: scopes });
+        } else {
+            console.info('No respuesta en facebookLogin');
+        }
+    });
+}
+
+var facebookLogout = function() {
+    FB.getLoginStatus(function(response) {
+        if(response.status === 'connected') {
+            if(confirm('¿Está seguro(a) de que desea cerrar sesión?')) {
+                FB.logout(function(response) {
+                    // Cerró sesión
+                });
+            } else {
+                return false;
+            }
+        }
+    }); 
 }
