182e69941b0e11b23ef3d0cef7e151b2b7bb4e67
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-04 21:29:03 +0000

    testing inappbrowser again

--- appCanchaLaPrimaera/platforms/browser/platform_www/cordova_plugins.js
+++ appCanchaLaPrimaera/platforms/browser/platform_www/cordova_plugins.js
@@ -7,13 +7,29 @@ module.exports = [
         "clobbers": [
             "facebookConnectPlugin"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-facebook4": "1.8.0"
+    "cordova-plugin-facebook4": "1.8.0",
+    "cordova-plugin-inappbrowser": "1.7.1"
 }
 // BOTTOM OF METADATA
 });
