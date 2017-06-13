6352b2af5bc396a4f74cd562c96eb4f97f26070b
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-13 16:53:23 +0000

    Prueba pregunta desea salir

--- appCanchaLaPrimaera/platforms/browser/www/js/home.js
+++ appCanchaLaPrimaera/platforms/browser/www/js/home.js
@@ -2,45 +2,17 @@
 /* global localStorage */
 /* global toastr */
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        //this.receivedEvent('deviceready');
-        document.addEventListener("backbutton", preguntarSiSalir, false);
-        
-        function preguntarSiSalir() {
-            $('#modalPreguntarSiSalir').modal('open');
-        }
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-       /* var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-        */
-        
-        
-    }
-};
-
-app.initialize();
-
 
 $(document).ready(function() {
+    
+    document.addEventListener(
+        "backbutton", 
+        function() {
+            $('#modalPreguntarSiSalir').modal('open');
+        },
+        false
+    );
+     
     if (typeof(Storage) !== "undefined") {
         var nombreUsuario = localStorage.getItem('NombreUsuario');
         if(nombreUsuario == undefined || nombreUsuario == "" || nombreUsuario == 'undefined') {
