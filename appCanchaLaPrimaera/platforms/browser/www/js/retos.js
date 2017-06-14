4536480375fb81cb24e96b36d1be3e5273732706
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-14 19:41:01 +0000

    Lista de retos

--- appCanchaLaPrimaera/platforms/browser/www/js/retos.js
+++ appCanchaLaPrimaera/platforms/browser/www/js/retos.js
@@ -18,6 +18,9 @@ function listarRetos() {
             var nombre = localStorage.getItem('Nombre');
             var apellidos = localStorage.getItem('Apellidos');
             if(result != false) {
+                $('#ulRetos').attr('class', 'collapsible');
+                $('#ulRetos').attr('data-collapsible', 'accordion');
+                $('.collapsible').collapsible();
                 $.each(result, function() {
                     var fecha = this.Fecha.split('-');
                     var li = document.createElement('li');
@@ -25,7 +28,7 @@ function listarRetos() {
                     divHeader.setAttribute("class", "collapsible-header");
                     $(divHeader).html('<i class="material-icons">compare_arrows</i><b>Fecha:</b> '
                     + this.NombreDia + ' ' + fecha[2] + '-' + fecha[1] + '-' + fecha[0]
-                    + ' <b>Hora:</b> ' + this.IdHoraReservable);
+                    + ' <b>Hora:</b> ' + convertirHora(this.Hora));
                     var divBody = document.createElement('div');
                     divBody.setAttribute('class', 'collapsible-body');
                     $(divBody).html("<label class='labelInfo'><b>Responsable: </b>" + nombre + " " + apellidos + "</label>"
@@ -45,4 +48,49 @@ function listarRetos() {
             toastr.error('Error de conexión con la base de datos');
         }
     });
+}
+
+function convertirHora(hora) {
+    switch (hora) {
+        case '12':
+            hora += ':00md';
+            break;
+        case '13':
+            hora = '1:00pm';
+            break;
+        case '14':
+           hora = '2:00pm';
+            break;
+        case '15':
+            hora = '3:00pm';
+            break;
+        case '16':
+            hora = '4:00pm';
+            break;
+        case '17':
+            hora = '5:00pm';
+            break;
+        case '18':
+            hora = '6:00pm';
+            break;
+        case '19':
+            hora = '7:00pm';
+            break;
+        case '20':
+            hora = '8:00pm';
+            break;
+        case '21':
+            hora = '9:00pm';
+            break;
+        case '22':
+            hora = '10:00pm';
+            break;
+        case '23':
+            hora = '11:00pm';
+            break;
+        default:
+            hora += ':00am';
+    }
+    return hora;
+        
 }
