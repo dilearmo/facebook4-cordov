bb4f1fddf558bf958a39c1effb9a0b15e3cfa1b0
DiLeArMo <DiLeArMo@hotmail.com>
2017-06-21 18:16:15 +0000

    Pruebas fondo de pantalla

--- appCanchaLaPrimaera/www/js/pantallaInicial.js
+++ appCanchaLaPrimaera/www/js/pantallaInicial.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
     if (typeof(Storage) !== "undefined") {
         var nombreUsuario = localStorage.getItem('NombreUsuario');
         if(nombreUsuario != undefined && nombreUsuario != "") {
-            window.location.href = 'home.html';
+            $('html, body').fadeOut(1500, function() {
+                window.location.href = 'home.html';
+            });
         } else {
-             window.location.href = 'inicio.html';
+            $('html, body').fadeOut(1500, function() {
+                window.location.href = 'inicio.html';
+            });
+             //window.location.href = 'inicio.html';
         }
     } else {
         toastr.info("Lo sentimos,<br>su teléfono no es<br>compatible con esta<br>aplicación");
